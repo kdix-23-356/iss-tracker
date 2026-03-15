33f2be3dc310aa8b7940e16eb41c5b31995e36f4
霜田湊太 <shimodasota@mac-book-air.local>
2026-03-15 17:56:13 +0900

    動作の安定性を確保する修正

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// ★ Cesium Widgets のCSSを必ず一度だけ読み込む
+import 'cesium/Build/Cesium/Widgets/widgets.css'
+
+// ★ 実行時保険：ViteのBASE_URLをグローバルへ
+declare global {
+  interface Window { CESIUM_BASE_URL?: string }
+}
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+window.CESIUM_BASE_URL = import.meta.env.BASE_URL!
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
