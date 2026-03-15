6ebcf614e752fdad2e856168ddaea9fd2af1f214
霜田湊太 <shimodasota@mac-book-air.local>
2026-03-15 16:49:10 +0900

    Cesiumのビルドエラー修正のためプラグインを導入

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import cesium from 'vite-plugin-cesium'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/iss-tracker/',
-  plugins: [react()],
+  plugins: [react(), cesium()],
 })
